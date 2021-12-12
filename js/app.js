82b0d1391b5ec02faeaa452b17676db9d0938417
Danilo Medeiros <danilo.medeiros801@gmail.com>
2021-12-12 14:45:22 -0300

    check

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@
 
 const add = document.querySelector(".input-add");
 const add_btn = document.querySelector(".btn-add");
-const del_button = `<button type="submit">
+const del_button = `<button class="delete" type="submit">
               <i class="fa fa-trash"></i>
             </button>`;
 
@@ -28,6 +28,23 @@ const createItem = () => {
     alert("Adicione uma tarefa!");
     items.remove();
   }
+
+  var current_tasks = document.querySelectorAll(".delete");
+  for (var i = 0; i < current_tasks.length; i++) {
+    current_tasks[i].addEventListener("click", function () {
+      this.parentNode.remove();
+    });
+  }
+
+  var tasks = document.querySelectorAll(".task");
+  for (var i = 0; i < tasks.length; i++) {
+    tasks[i].addEventListener("click", function () {
+      this.classList.toggle("completed");
+    });
+  }
+  
+
+
 };
 
  
